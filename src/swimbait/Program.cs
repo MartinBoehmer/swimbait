e5b2fe85adee50f3cab7c6361323209860f563fa
neutmute <neutmute@users.noreply.github.com>
2016-03-06 06:44:44 +1100

    got a response from plex

--- src/swimbait/Program.cs
+++ src/swimbait/Program.cs
@@ -13,47 +13,46 @@ namespace swimbait
         static void Main(string[] args)
         {
 
-            IPEndPoint LocalEndPoint = new IPEndPoint(IPAddress.Any, 60000);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 60000);
 
             var endpointPort = 1900;
             var endpointIp = "239.255.255.250";
-            IPEndPoint MulticastEndPoint = new IPEndPoint(IPAddress.Parse(endpointIp), endpointPort);
+            IPEndPoint multicastEndPoint = new IPEndPoint(IPAddress.Parse(endpointIp), endpointPort);
 
-            Socket UdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-          //  UdpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            UdpSocket.Bind(LocalEndPoint);
-            //UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(MulticastEndPoint.Address, IPAddress.Any));
-            //UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
-            //UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastLoopback, true);
+          //  udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpSocket.Bind(localEndPoint);
+            //udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastEndPoint.Address, IPAddress.Any));
+            //udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+            //udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastLoopback, true);
 
             Console.WriteLine("UDP-Socket setup done...\r\n");
 
-            string SearchString = $@"M-SEARCH * HTTP/1.1
-HOST:{endpointIp}:{endpointPort}
-MAN:""ssdp:discover""
-MX:2
+            string searchString = $@"M-SEARCH * HTTP/1.1
+Host: {endpointIp}:{endpointPort}
+Content-Length: 0
+MAN: ""ssdp:discover""
+MX: 2
 ST: urn:schemas-upnp-org:device:MediaServer:1
 
 ";
 
-            UdpSocket.SendTo(Encoding.UTF8.GetBytes(SearchString), SocketFlags.None, MulticastEndPoint);
+            udpSocket.SendTo(Encoding.UTF8.GetBytes(searchString), SocketFlags.None, multicastEndPoint);
 
             Console.WriteLine("M-Search sent...\r\n");
 
-            byte[] ReceiveBuffer = new byte[64000];
-
-            int ReceivedBytes = 0;
+            byte[] receiveBuffer = new byte[64000];
 
             while (true)
             {
-                if (UdpSocket.Available > 0)
+                if (udpSocket.Available > 0)
                 {
-                    ReceivedBytes = UdpSocket.Receive(ReceiveBuffer, SocketFlags.None);
+                    var receivedBytes = udpSocket.Receive(receiveBuffer, SocketFlags.None);
 
-                    if (ReceivedBytes > 0)
+                    if (receivedBytes > 0)
                     {
-                        Console.WriteLine(Encoding.UTF8.GetString(ReceiveBuffer, 0, ReceivedBytes));
+                        Console.WriteLine(Encoding.UTF8.GetString(receiveBuffer, 0, receivedBytes));
                     }
                 }
             }
