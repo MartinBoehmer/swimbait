2703e6c2fee043d555f987564f296ebd0ea9b3c5
neutmute <neutmute@users.noreply.github.com>
2016-05-01 07:33:20 +1000

    discovery almost works

---
+++ src/server/Controllers/MusicCastNetworkController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Swimbait.Server.Controllers.Responses;
+
+namespace Swimbait.Server.Controllers
+{
+   
+
+    [Route("MusicCastNetwork")]
+    public class MusicCastNetworkController : Controller
+    {
+        public MusicCastNetworkController()
+        {
+
+        }
+        
+        [HttpGet("InitialJoinComplete")]
+        public IActionResult InitialJoinComplete()
+        {
+            var response = new BasicResponse();
+            return new ObjectResult(response);
+        }
+
+    }
+
+}
