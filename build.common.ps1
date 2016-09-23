ccf57b3ae51a9329dbac2052723b964b2bf6919b
neutmute <neutmute@users.noreply.github.com>
2016-09-24 06:42:41 +1000

    fancy build

---
+++ build.common.ps1
@@ -0,0 +1,87 @@
+$Script:UseWriteHost = $true
+#http://patorjk.com/software/taag/#p=display&f=Standard&t=Build (standard font)
+
+$Global:asciiArtBuild = @"
+  ____        _ _     _  
+ |  _ \      (_) |   | | 
+ | |_) |_   _ _| | __| | 
+ |  _ <| | | | | |/ _  | 
+ | |_) | |_| | | | (_| | 
+ |____/ \__,_|_|_|\__,_| 
+ 
+"@
+
+if(!$Global:ColorScheme) {
+    $Global:ColorScheme = @{
+        "Banner"=[ConsoleColor]::Cyan
+        "RuntimeName"=[ConsoleColor]::Yellow
+        "Help_Header"=[ConsoleColor]::Yellow
+        "Help_Switch"=[ConsoleColor]::Green
+        "Help_Argument"=[ConsoleColor]::Cyan
+        "Help_Optional"=[ConsoleColor]::Gray
+        "Help_Command"=[ConsoleColor]::DarkYellow
+        "Help_Executable"=[ConsoleColor]::DarkYellow
+        "ParameterName"=[ConsoleColor]::Cyan
+        "Warning" = [ConsoleColor]::Yellow
+    }
+}
+
+function _WriteOut {
+    param(
+        [Parameter(Mandatory=$false, Position=0, ValueFromPipeline=$true)][string]$msg,
+        [Parameter(Mandatory=$false)][ConsoleColor]$ForegroundColor,
+        [Parameter(Mandatory=$false)][ConsoleColor]$BackgroundColor,
+        [Parameter(Mandatory=$false)][switch]$NoNewLine)
+
+    if($__TestWriteTo) {
+        $cur = Get-Variable -Name $__TestWriteTo -ValueOnly -Scope Global -ErrorAction SilentlyContinue
+        $val = $cur + "$msg"
+        if(!$NoNewLine) {
+            $val += [Environment]::NewLine
+        }
+        Set-Variable -Name $__TestWriteTo -Value $val -Scope Global -Force
+        return
+    }
+
+    if(!$Script:UseWriteHost) {
+        if(!$msg) {
+            $msg = ""
+        }
+        if($NoNewLine) {
+            [Console]::Write($msg)
+        } else {
+            [Console]::WriteLine($msg)
+        }
+    }
+    else {
+        try {
+            if(!$ForegroundColor) {
+                $ForegroundColor = $host.UI.RawUI.ForegroundColor
+            }
+            if(!$BackgroundColor) {
+                $BackgroundColor = $host.UI.RawUI.BackgroundColor
+            }
+
+            Write-Host $msg -ForegroundColor:$ForegroundColor -BackgroundColor:$BackgroundColor -NoNewLine:$NoNewLine
+        } catch {
+            $Script:UseWriteHost = $false
+            _WriteOut $msg
+        }
+    }
+}
+
+function _WriteConfig{
+param(
+        [Parameter(Mandatory=$true,Position=0)]$name,
+        [Parameter(Mandatory=$true,Position=1)]$value)
+		
+	_WriteOut -NoNewline -ForegroundColor $Global:ColorScheme.ParameterName "${name}: "
+	_WriteOut "$value"
+
+}
+
+function _WriteDiagnostics(){
+
+	_WriteConfig "PATH" "$env:PATH"
+
+}
--- build.ps1
+++ build.ps1
@@ -1,3 +1,32 @@
-dotnet build .\src\server
-dotnet build .\src\jig
-dotnet build client
+. ".\build.common.ps1"
+
+$thisFolder = (Get-Item -Path ".\" -Verbose).FullName
+
+function build{
+
+    $projectsToBuild = @(
+		".\src\server";
+	    ".\src\jig";
+        ".\client";
+    )
+
+    _WriteOut -ForegroundColor $ColorScheme.Banner $asciiArtBuild
+    
+    foreach($project in $projectsToBuild) {
+    
+	    _WriteConfig "Restore" "$project"
+        dotnet restore $project
+        
+        _WriteConfig "Build" "$project"
+        dotnet build $project
+	
+	    if (!$LastExitCode -eq 0)
+	    {
+		    Write-Host "Build failed for '$project'"
+		    exit 1
+	    }
+    }
+	
+}
+
+build
